f992c6d44576199e43c4742ecc23c368fe87d376
savargb <savargb123@gmail.com>
2026-05-31 12:52:40 +0545

    v3

---
+++ mathematicaloperation.py
@@ -0,0 +1,12 @@
+import math
+print('The floor and ceilng value of 23.56 are: ' +
+       str(math.ceil(23.56)) + ', ' + str(math.floor(23.56)))
+
+x = 10
+y = -15
+print('The value of x after copying the sign from y is: ' +
+str(math.copysign(x,y)))
+
+print('absolute value of -96 and 56 are: ' + str(math.fabs(-96)) + ', ' + str(math.fabs(56)))
+
+print('The GCD of 26 and 56 : '+ str(math.gcd(24, 56)))
